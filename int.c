1ce06464eab5b4a163bfa6f6e6bbbdb1a9d989d5
sanjay36 <31504582+sanjay36@users.noreply.github.com>
2018-01-23 10:57:44 +0000

    Create int.c

---
+++ int.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n;
+int count=0;
+printf("enter the number");
+scanf("%d",&n);
+while(i!=0)
+{
+n/=0;
+count++;
+}
+printf("the number of digits: %d",count);
+return 0;
+}
