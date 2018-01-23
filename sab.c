ab4718236faa3dfaad4f92ca84f73333d0bd7f05
sanjay36 <31504582+sanjay36@users.noreply.github.com>
2018-01-23 11:17:59 +0000

    Create sab.c

---
+++ sab.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter the mumber");
+scanf("%d",&num);
+if(prime num==0)
+printf("number is not prime");
+else
+printf("the number is prime");
+}
+int prime (int no);
+{
+int i;
+for(i=2;i<no;i++)
+if (no%i==0)
+
+}
