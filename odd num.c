8bcbc906e453d441ec35b099885d6dc2c2f160b8
sanjay36 <31504582+sanjay36@users.noreply.github.com>
2018-01-23 11:20:13 +0000

    Create odd num.c

---
+++ odd num.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int num,m =20,n=40;
+    printf("Print Odd Numbers n:\n");
+    for (num = m; num <= n; num++)
+        {
+          if (num % 2 == 1)
+          printf ("%d ",num);
+          return 0;
+         }            
+}
