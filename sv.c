71673bff1cff4f9cc095e3c0065ca48a78951c98
sanjay36 <31504582+sanjay36@users.noreply.github.com>
2018-01-23 11:28:30 +0000

    Create sv.c

---
+++ sv.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main()
+{
+    int num, on, rem, res= 0;
+
+    printf("Enter a three digit integer: ");
+    scanf("%d", &num);
+
+    on = num;
+
+    while (on != 0)
+    {
+        rem = on%10;
+        res += rem*rem*rem;
+        on /= 10;
+    }
+
+    if(res == num)
+        printf("%d is an Armstrong number.",number);
+    else
+        printf("%d is not an Armstrong number.",number);
+
+    return 0;
+}
