31ae1c596b73d2a10a9aaf02fe665e7644e816c2
sanjay36 <31504582+sanjay36@users.noreply.github.com>
2018-01-23 11:25:50 +0000

    Create lal.c

---
+++ lal.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+voic main()
+{
+int prime1=5,prime2=4;
+printf("enter the integer number");
+scanf("%d",&prime);
+if(prime1%5=1)
+printf("enter the prime1 value");
+else(prime2%4=1)
+printf("enter the prime2 value");
+}
