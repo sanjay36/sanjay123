b3f36c56539d82ca9b09e3e6aec6156bb862fc22
sanjay36 <31504582+sanjay36@users.noreply.github.com>
2018-01-25 10:12:27 +0000

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,12 @@
+include<stdio.h>
+void main()
+{
+	int n,fact=1,i;
+	printf("enter any integer");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		fact=fact*i;
+	}
+	printf("factoial of given no is%d",fact);
+}
