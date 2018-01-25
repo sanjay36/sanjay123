aefff8ccd1d8c8dda2ec508e7582d7b14a509d1e
sanjay36 <31504582+sanjay36@users.noreply.github.com>
2018-01-25 10:27:50 +0000

    Create gir.c

---
+++ gir.c
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+ void main()
+{
+int a[10],i,n;
+printf("enter array element");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("\narray %d",i);
+}
+int low=a[0],high=n,mid;
+if(low<high)
+{
+	mid=(low+high)/2;
+	printf("\n%d",mid);
+}
