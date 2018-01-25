f0ea107cb6f31aad914a8ab6a2640bb7f7964a26
sanjay36 <31504582+sanjay36@users.noreply.github.com>
2018-01-25 10:32:29 +0000

    Create rrr.c

---
+++ rrr.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+ int a[max size];
+ printf("\n Enter the numbers..")
+ scanf("%d",&n);
+ for(i=0;i<=n;i+=+)
+ {
+  scanf("d',&a[i]);
+  }
+  printf("\n The elements are...");
+  for(j=0;j<n;j++)
+  {
+   printf("\n The elements with their index value...");
+   return 0;
+   }
+   }
+
