62c5abd105d38873d562ab615a82b94ccd1e0fe2
sanjay36 <31504582+sanjay36@users.noreply.github.com>
2018-01-25 10:48:14 +0000

    Create ssss.c

---
+++ ssss.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	char words[200];
+	int spaces=0;
+	int flag=0,i;
+	clrscr();
+	printf("enter a line of text:\n");
+	if(words[i]=='');
+	{
+		++spaces;
+		flag=0;
+	}
+	printf("\n number of white spacesL:%d",spaces);
+	getch();
+	}
+
