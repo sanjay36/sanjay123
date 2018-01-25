6daacc1a886a1dacc24ce00e722c86d8feda1d11
sanjay36 <31504582+sanjay36@users.noreply.github.com>
2018-01-25 10:49:53 +0000

    Create friend.c

---
+++ friend.c
@@ -0,0 +1,14 @@
+#incude<stdio.h>
+ void main()
+{
+int i,s;
+char a[50];
+printf("enter the string");
+scanf("%s",&a[i]);
+for(i=0;a[i]<=0;i++)
+{
+if(a[i]==' ')
+s++;
+}
+printf("enter the number of space is:%d",s);
+}
