a5745582443395e7a9d31fe6cd070dda1372703b
sanjay36 <31504582+sanjay36@users.noreply.github.com>
2018-01-26 15:02:11 +0530

    Create vcc.c

---
+++ vcc.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int n,i;
+scanf("%d",&n);
+int a[n];
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+printf("\t %d",a[i]);
+}
+int large=a[0];
+for(i=1;i<n;i++)
+{
+if(large<a[i])
+{
+large=a[i];
+}
+}
+printf("\n large number %d",large);
+
+}
