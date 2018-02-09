f413ded712197c734277f25bd3549b7a0848fd0b
sanjay36 <31504582+sanjay36@users.noreply.github.com>
2018-02-09 14:35:57 +0530

    Create iso.c

---
+++ iso.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int i,j;
+char a[10],b[10];
+scanf(%s,&a);
+scanf(%s,&b);
+j=strlen(a);
+for(i=0;i<=j;i++)
+{if (a[i]==a[i+1]&&b[i]==b[i+1])
+printf("yes");
+}}
