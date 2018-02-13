c705052644053fcdd844605e157d079433997835
sanjay36 <31504582+sanjay36@users.noreply.github.com>
2018-02-13 14:12:53 +0530

    Create hunt 1.c

---
+++ hunt 1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int j,i,k;
+scanf("%d",&j);
+int a[20];
+for(i=0;i<j;i++)
+{scanf("%d",&a[i]);}
+for(i=0;i<j;i++)
+{
+    for(k=i+1;k<j;k++)
+{
+if(a[i]==a[k])
+printf("%d ",a[i]);
+return 0;}
+}
+}
