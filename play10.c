f1cd28077cf16a6668d0e2cdd12d6bcae7f9433e
sanjay36 <31504582+sanjay36@users.noreply.github.com>
2018-03-08 16:19:01 +0530

    Rename play9.c to play10.c

---
+++ play10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void main()
+{
+    int a,b,i,c;
+    scanf("%d %d", &a, &b);
+    a+=1;
+    while (a< b)
+    {
+        c=0;
+
+        for(i=2;i<=a/2;++i)
+        {
+            if(a%i==0)
+            {
+                c = 1;
+                break;
+            }
+        }
+
+        if (c==0)
+            printf("%d",a);
+
+        ++a;
+    }
+
+}
