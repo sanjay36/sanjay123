53ba23b3986a521259ba285bc3b94b6eb4a8ba80
sanjay36 <31504582+sanjay36@users.noreply.github.com>
2018-03-13 10:36:17 +0000

    Rename sam 3.c to sam4.c

---
+++ sam4.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+void main()
+{
+    int i,a,b;
+    double l,u,min1,min2,diff;
+    scanf("%lf %lf",&l,&u);
+    a=l;
+    b=u;
+    min1=l-a;
+    min2=u-b;
+    a=a*60;
+    b=b*60;
+    min1=min1*100;
+    min2=min2*100;
+    diff=a+min1-b-min2;
+    if(diff<0)
+    {
+    printf("%.0lf mins",-(diff));
+    }
+    else
+    {
+        printf("%.0lf mins",diff);
+    }
+}
